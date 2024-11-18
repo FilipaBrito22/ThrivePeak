1d09b84d2b0b9f04249e139e10741585a99a7912
Filipa Brito <153316607+FilipaBrito22@users.noreply.github.com>
2024-11-18 20:08:36 +0000

    Update db_connection.php

--- db_connection.php
+++ db_connection.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "company";
+$servername = "thrivepeakcompany.database.windows.net"; // Your Azure SQL server name
+$username = "CloudSAc843b116"; // The username you set for your Azure SQL database
+$password = "Filipa1602#"; // The password you set for your Azure SQL database
+$dbname = "company"; // The name of your database
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
