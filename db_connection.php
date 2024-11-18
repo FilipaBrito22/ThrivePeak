42692fe22030d57db5868762e52dd1dc02390e94
Filipa Brito <153316607+FilipaBrito22@users.noreply.github.com>
2024-11-18 22:35:07 +0000

    Update db_connection.php

--- db_connection.php
+++ db_connection.php
@@ -7,7 +7,6 @@ $password = "Filipa1602#"; // The password you set for your Azure SQL database
 try {
     $conn = new PDO("sqlsrv:server=$serverName;Database=$database", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully!";
 } catch (PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
 }
